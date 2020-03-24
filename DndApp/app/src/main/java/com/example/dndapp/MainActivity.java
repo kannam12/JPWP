3e37e0ed72f72b9f98f87349008318a979b3ab42
kannam12 <kannam12@github.com>
2020-03-24 21:00:16 +0100

    v 0.0

---
+++ DndApp/app/src/main/java/com/example/dndapp/MainActivity.java
@@ -0,0 +1,32 @@
+package com.example.dndapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void goNextMain(View view) {
+        Intent intent;
+        switch(view.getId()) {
+            case R.id.goAheadBtn:
+                intent = new Intent(MainActivity.this, LogActivity.class);
+                startActivity(intent);
+                break;
+
+            case R.id.appInfoBtn:
+                intent = new Intent(MainActivity.this, InfoActivity.class);
+                startActivity(intent);
+                break;
+
+        }
+    }
+}
